be22f6af704ef896fde6bbd4611764500e5b8876
Yuriy Bevov <yuriybevov@gmail.com>
2025-03-02 20:12:31 +0300

    Добавил мобильное меню

--- dist/assets/index.pug-D8FF_K9P.js
+++ dist/assets/index.pug-D8FF_K9P.js
@@ -5261,36 +5261,17 @@ function Thumb(_ref) {
   });
 }
 const slider = document.querySelector(".top-banner-slider");
-console.log(slider);
 if (slider) {
-  slider.querySelector(".swiper-pagination");
   const btnNext = slider.querySelector(".swiper-button-next");
   const btnPrev = slider.querySelector(".swiper-button-prev");
   new Swiper(slider, {
-    modules: [Navigation, Pagination, Autoplay],
+    modules: [Navigation],
     slidesPerView: 1,
     spaceBetween: 20,
-    // effect: "fade",
-    // autoplay: {
-    // 	delay: 5000,
-    // 	disableOnInteraction: false,
-    // },
-    // breakpoints: {
-    // 	640: {
-    // 		slidesPerView: 2,
-    // 	},
-    // 	960: {
-    // 		slidesPerView: 3,
-    // 	},
-    // },
     navigation: {
       nextEl: btnNext ? btnNext : null,
       prevEl: btnPrev ? btnPrev : null
     }
-    // pagination: {
-    // 	el: pagination ? pagination : null,
-    // 	dynamicBullets: true,
-    // },
   });
 }
 const sliders = document.querySelectorAll(".base-cards-slider");
@@ -5367,8 +5348,8 @@ if (mainSlider) {
 const accordeons = document.querySelectorAll(".accordeon");
 if (accordeons) {
   accordeons.forEach((accordeon) => {
-    const items = accordeon.querySelectorAll(".accordeon-header");
-    items.forEach((item) => {
+    const items2 = accordeon.querySelectorAll(".accordeon-header");
+    items2.forEach((item) => {
       item.addEventListener("click", () => {
         item.parentNode.classList.toggle("expanded");
       });
@@ -9335,6 +9316,50 @@ _forEachName("x,y,z,top,right,bottom,left,width,height,fontSize,padding,margin,p
 gsap.registerPlugin(CSSPlugin);
 var gsapWithCSS = gsap.registerPlugin(CSSPlugin) || gsap;
 gsapWithCSS.core.Tween;
+const items = document.querySelectorAll(".top-menu-inner-opener");
+if (items.length) {
+  items.forEach((item) => {
+    item.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      evt.currentTarget.parentNode.parentNode.classList.toggle("expanded");
+    });
+  });
+}
+const opener = document.querySelector(".mobile-menu-opener");
+const closer = document.querySelector(".mobile-menu-closer");
+const menu = document.querySelector(".mobile-menu");
+if (opener && closer && menu) {
+  const tl = gsapWithCSS.timeline().pause();
+  tl.fromTo(
+    ".mobile-menu",
+    { opacity: 0 },
+    { display: "block", opacity: 1, duration: 0.4 }
+  );
+  tl.fromTo(
+    ".mobile-menu__wrapper",
+    { x: "-100vw", opacity: 0 },
+    { opacity: 1, x: 0, duration: 0.4 },
+    "-=.2"
+  );
+  const onClickOpenMenu = () => {
+    tl.play();
+    document.addEventListener("click", onOverlayClickHandler);
+    window.addEventListener("keydown", onEscClickHandler);
+  };
+  const onOverlayClickHandler = (evt) => {
+    if (evt.target.classList.contains("mobile-menu")) onClickCloseMenu();
+  };
+  const onEscClickHandler = (evt) => {
+    if (evt.key === "Escape" || evt.code === 27) onClickCloseMenu();
+  };
+  const onClickCloseMenu = () => {
+    tl.reverse();
+    document.removeEventListener("click", onOverlayClickHandler);
+    window.removeEventListener("keydown", onEscClickHandler);
+  };
+  closer.addEventListener("click", onClickCloseMenu);
+  opener.addEventListener("click", onClickOpenMenu);
+}
 window.addEventListener("load", () => {
   const btn = document.createElement("button");
   btn.classList.add("scroll-up-btn");
--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,5 @@ import "../pug/components/_ui/sliders/top-banner/top-banner-slider";
 import "../pug/components/_ui/sliders/base-cards-slider/base-cards-slider";
 import "../pug/components/_ui/sliders/staff-preview-slider/staff-preview-slider";
 import "../pug/components/_ui/accordeon/accordeon";
+import "../pug/components/_ui/nav/top-menu/top-menu";
 import "./modules/scroll-up-btn";
